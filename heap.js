eca6117c0e5ed59c69399587674102595fba7b29
Ankit Chohan <Sidhantuppal08@gmail.com>
2023-04-25 18:27:39 +0530

    Commit

--- heap.js
+++ heap.js
@@ -17,7 +17,7 @@ class MaxBinaryHeap{
             this.value[Index] = parentElement;
             Index = parentIndex;//again made the index where it is parent
         }
-        return this.value 
+        return this.value;
     }
     extractMax(){
         //Swapped
@@ -36,26 +36,27 @@ class MaxBinaryHeap{
         const length = this.value.length;
         const element = this.value[0];
         while(true){
-            let leftChild = 2*rootIndex+1;
+            let leftChild = 2*rootIndex+1; //make sure that that it is not out of bonds
             let rightChild = 2*rootIndex+2;
             let left ,right ,swap=null;
 
-            if(leftChild<length){
-                left = this.value[leftChild]
-                if(left>element)  {
-                    swap =leftChild; 
+            if(leftChild<length){ //through this 
+                left = this.value[leftChild]//value of the left Child of the first
+                if(left>element)  {//if it is greater than the element than swap at the end
+                    swap =leftChild; //just acting as a indicator that we need to swap
                 }             
             }
-            if(rightChild<length){
-                right = this.value[rightChild]
-                if((swap===null&&right>element) || (swap!==null&& right>left)){
+            if(rightChild<length){//through this out of bound
+                right = this.value[rightChild] //value of the right child of the first
+                if((swap===null&&right>element) || (swap!==null&& right>left)){//condition 1 : if the left has not swapped that means that left child was less than element
+                    //condition 2 : swapped has occured but the right is still greater than left and element
                     swap = rightChild;
                 }
             }
-            if(swap===null) break;
+            if(swap===null) break; //if there is no swap that means there is no need to be in the loop, postion is correct hence loop out of it
             //swap
-            this.value[rootIndex] = this.value[swap] 
-            this.value[swap] = element;
+            this.value[rootIndex] = this.value[swap] //making the intial value of the equal to the the value where there is swapped index
+            this.value[swap] = element; //making the swapped value the intial value 
         }
     }
 }
